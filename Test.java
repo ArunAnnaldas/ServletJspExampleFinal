64400e49869d4f02a8eeb8fba3c148493fbfd075
ArunAnnaldas <cmeatarun1988@gmail.com>
2025-01-24 09:15:07 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,1 +1,12 @@
 java -jar selenium-server-4.15.0.jar standalone --selenium-manager true --session-timeout 999999 --session-request-timeout 999999 --max-sessions 5
+
+
+  
+Playwright playwright Playwright.create();
+Browser browser = playwright.chromium().launch(new BrowserType. LaunchOptions().setHeadless (false)); Page page browser.newPage(); page.navigate("https://the-internet.herokuapp.com/javascript_alerts");
+page.onDialog(dialog -> {
+});
+dialog.accept("Testing");
+System.out.println(dialog.message());
+page.locator("text=Click for JS Alert").click(); page.locator("text=Click for JS Confirm").click();
+page.locator("text=Click for JS Prompt").click();
